21d4e51e8b8efb8f2e0461888989367a488789bc
ta-design2021 <takenori.aino@gmail.com>
2023-11-04 01:52:04 +0900

    選択項目をアクティブハッシュで記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
+  # before_actionを使い各アクションの事前処理として設定
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+  # sign_up（新規登録の処理）に対して、各キーのパラメーターを新たに許可
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :gender_id, :birth_date, :phone_number, :postal_code, :prefecture_id, :city, :street_address, :membership_plan_id])
+  # end
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :gender_id, :birth_date, :phone_number, :postal_code, :prefecture_id, :city, :street_address, :membership_plan_id])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :gender_id, :birth_date, :phone_number, :membership_plan_id])
   end
 end
--- app/models/membership_plan.rb
+++ app/models/membership_plan.rb
@@ -1,2 +1,13 @@
-class MembershipPlan < ApplicationRecord
+# class MembershipPlan < ApplicationRecord
+# end
+class MembershipPlan < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '月額プラン ¥600（税込）/1ヶ月半' },
+    { id: 3, name: '年額プラン ¥6,600（税込）/1年' }
+  ]
+
+    include ActiveHash::Associations
+    has_one :user
+
 end
